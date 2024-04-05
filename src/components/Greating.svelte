02a3aae7df5e8f85e4cab8fd57ad915806dc4187
G-ROOT <123758906+CODE-G-ROOT@users.noreply.github.com>
2024-04-04 21:49:19 -0500

    feat: :sparkles: Nuevo componets, React, Svelte y audio
    se han agregado y configurado la visualización de las cards, además se han arreglado algunos estilos.  Se agregó React y Svelte para la creación de ciertos componentes.
    
    Queda pendiente configurar el audio para que cambie según el id de la canción a la que se dé click

---
+++ src/components/Greating.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greating = "";
+
+  if (currentHour < 12) {
+    greating = "Buenos Dias";
+  } else if (currentHour < 18 ) {
+    greating = "Buenas Tardes";
+  } else {
+    greating = "Buenas Noches"
+  }
+
+</script>
+
+<h1 class="text-3xl font-bold">{greating}</h1>
---
+++ src/components/Player.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useRef, useState } from "react";
+
+const Pasue = () => (
+	<svg
+		role='img'
+		aria-hidden='true'
+		viewBox='0 0 16 16'
+		width='16'
+		height='16'
+	>
+		<path d='M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z'></path>
+	</svg>
+);
+
+const Play = () => (
+	<svg
+		role='img'
+		aria-hidden='true'
+		viewBox='0 0 16 16'
+		width='16'
+		height='16'
+	>
+		<path d='M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z'></path>
+	</svg>
+);
+
+export const Player = () => {
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [currentSong, setCurrentSong] = useState(null);
+	const audioRef = useRef();
+
+	useEffect (() => {
+		audioRef.current.src = `/music/1/01.mp3`;
+	}, []);
+
+	const handleClick = () => {
+		if (isPlaying) {
+			audioRef.current.pause();
+		} else {
+			audioRef.current.play();
+			audioRef.current.volume = 0.5
+		}
+
+		setIsPlaying(!isPlaying);
+	}
+
+
+	return (
+		<div className='flex flex-row justify-between w-full px-4 z-50'>
+			<div>current song</div>
+
+			<div className='grid place-content-center gap-4 flex-1'>
+				<div className='flex justify-center'>
+					<button
+						className='bg-white rounded-full p-2'
+						onClick={() => handleClick()}
+					>
+						{isPlaying ? <Pasue /> : <Play />}
+					</button>
+				</div>
+			</div>
+
+			<div className="grid place-content-center"></div>
+
+			<audio ref={ audioRef } />
+		</div>
+	);
+};
