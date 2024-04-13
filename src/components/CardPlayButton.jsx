8b3364584b5149de2c8d4f87708ba2aa06b10aeb
G-ROOT <123758906+CODE-G-ROOT@users.noreply.github.com>
2024-04-13 00:27:45 -0500

    feat: :sparkles: spotify clone

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Pause, Play } from "./Player";
 import { usePlayerStore } from "../store/playerStore";
 
-export function CardPlayButton({ id }) {
+export function CardPlayButton({ id, size = "small" }) {
 	const { isPlaying, setIsPlaying, currentMusic, setCurrentMusic } = usePlayerStore((state) => state);
 
 	const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
@@ -23,12 +23,13 @@ export function CardPlayButton({ id }) {
 
   };
 
+  const iconClassName = size === "small" ? "w-4 h-4" : "w-5 h-5";
 	return (
 		<button
 			onClick={handleClick}
-			className='card-play-button rounded-full bg-green-500 p-4 shadow-black/50 shadow-lg'
+			className='card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400 duration-75 ease-linear'
 		>
-			{isPlayingPlaylist ? <Pause /> : <Play />}
+			{isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
 		</button>
 	);
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -8,14 +8,28 @@ import {
 	VolumeSilence,
 } from "../icons/Sound";
 
-export const Pause = () => (
-	<svg role='img' aria-hidden='true' viewBox='0 0 16 16' width='16' height='16'>
+export const Pause = ({className}) => (
+	<svg 
+		role='img' 
+		aria-hidden='true' 
+		viewBox='0 0 16 16' 
+		width='16' 
+		height='16'
+		className={className}
+	>
 		<path d='M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z'></path>
 	</svg>
 );
 
-export const Play = () => (
-	<svg role='img' aria-hidden='true' viewBox='0 0 16 16' width='16' height='16'>
+export const Play = ({className}) => (
+	<svg 
+		role='img' 
+		aria-hidden='true' 
+		viewBox='0 0 16 16' 
+		width='16' 
+		height='16'
+		className={className}
+	>
 		<path d='M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z'></path>
 	</svg>
 );
@@ -27,7 +41,6 @@ const VolumeControl = () => {
 	const isVolumeSilence = volume < 0.1;
 
 	const handleClickVolume = () => {
-		console.log(isVolumeSilence);
 		if (isVolumeSilence) {
 			setVolume(previousVolumeRef.current);
 		} else {
@@ -75,7 +88,7 @@ const CurrentSong = ({ image, title, artists }) => {
 			className={`flex items-center gap-5 relative
 			overflow-hidden`}
 		>
-			<picture className='w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden'>
+			<picture className='w-14 h-14 bg-zinc-800 rounded-md shadow-lg overflow-hidden'>
 				<img src={image} alt={title} />
 			</picture>
 
@@ -83,6 +96,56 @@ const CurrentSong = ({ image, title, artists }) => {
 				<h3 className='font-semibold text-sm block'>{title}</h3>
 				<span className='text-xs opacity-80'>{artists?.join(", ")}</span>
 			</div>
+		</div>
+	);
+};
+
+const SongControl = ({ audio }) => {
+	const [currentTime, setCurrentTime] = useState(0);
+
+	useEffect(() => {
+		audio.current.addEventListener("timeupdate", hadleTimeUpdate);
+		return () => {
+			audio.current.removeEventListener("timeupdate", hadleTimeUpdate);
+		};
+	});
+
+	const hadleTimeUpdate = () => {
+		setCurrentTime(audio.current.currentTime);
+	};
+
+	const formatTime = (time) => {
+		if (time == 0) return "0:00";
+
+		const seconds = Math.floor(time % 60);
+		const minutes = Math.floor(time / 60);
+
+		return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+	};
+
+	const duration = audio?.current?.duration ?? 0;
+
+	return (
+		<div className='flex gap-x-2 text-xs pt-2'>
+			<span className='opacity-60 w-12 text-right'>
+				{[formatTime(currentTime)]}
+			</span>
+			<Slider
+				min={0}
+				max={audio?.current?.duration ?? 0}
+				value={[currentTime]}
+				className='w-[520px]'
+				// value={currentValue}
+				onValueChange={(value) => {
+					audio.current.currentTime = value;
+				}}
+			/>
+			<span className='opacity-60 w-12 text-left'>
+				{ duration 
+					? formatTime(duration)
+				  : '0:00'
+				}
+			</span>
 		</div>
 	);
 };
@@ -117,24 +180,26 @@ export const Player = () => {
 	};
 
 	return (
-		<div className='flex flex-row justify-between w-full px-4 z-50'>
-			<div>
+		<div className='flex flex-row justify-between w-full py-2 z-50'>
+			<div className="w-[171.4px] pl-2">
 				<CurrentSong {...currentMusic.song} />
 			</div>
 
 			<div className='grid place-content-center gap-4 flex-1'>
-				<div className='flex justify-center'>
+				<div className='flex justify-center items-center flex-col'>
 					<button
-						className='bg-white rounded-full p-2'
-						onClick={() => handleClick()}
+						className='bg-white w-min rounded-full p-2'
+						onClick={handleClick}
 					>
 						{isPlaying ? <Pause /> : <Play />}
 					</button>
+					<SongControl audio={audioRef} />
 				</div>
 			</div>
 
-			<div className='grid place-content-center'></div>
-			<VolumeControl />
+			<div className='grid place-content-center w-[171.4px]'>
+				<VolumeControl />
+			</div>
 			<audio ref={audioRef} />
 		</div>
 	);
